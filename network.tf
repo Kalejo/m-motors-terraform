a03d79234a576b254378594cc86e9ba8dd92b1be
Alexandro <38000075+Kalejo@users.noreply.github.com>
2026-09-14 22:32:06 +0200

    Ajout des sous-réseaux multi-AZ

--- network.tf
+++ network.tf
@@ -17,3 +17,79 @@ resource "aws_internet_gateway" "main" {
     Name = "${var.project_name}-igw"
   }
 }
+
+# Sous-réseau public de la zone A
+resource "aws_subnet" "public_a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "${var.aws_region}a"
+
+  tags = {
+    Name = "${var.project_name}-public-a"
+    Tier = "public"
+  }
+}
+
+# Sous-réseau public de la zone B
+resource "aws_subnet" "public_b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "${var.aws_region}b"
+
+  tags = {
+    Name = "${var.project_name}-public-b"
+    Tier = "public"
+  }
+}
+
+# Sous-réseau privé applicatif de la zone A
+resource "aws_subnet" "private_app_a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.11.0/24"
+  availability_zone       = "${var.aws_region}a"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "${var.project_name}-private-app-a"
+    Tier = "private-app"
+  }
+}
+
+# Sous-réseau privé applicatif de la zone B
+resource "aws_subnet" "private_app_b" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.21.0/24"
+  availability_zone       = "${var.aws_region}b"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "${var.project_name}-private-app-b"
+    Tier = "private-app"
+  }
+}
+
+# Sous-réseau privé de base de données de la zone A
+resource "aws_subnet" "private_db_a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.12.0/24"
+  availability_zone       = "${var.aws_region}a"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "${var.project_name}-private-db-a"
+    Tier = "private-db"
+  }
+}
+
+# Sous-réseau privé de base de données de la zone B
+resource "aws_subnet" "private_db_b" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.22.0/24"
+  availability_zone       = "${var.aws_region}b"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "${var.project_name}-private-db-b"
+    Tier = "private-db"
+  }
+}
