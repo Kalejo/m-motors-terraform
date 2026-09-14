995fe3cdef04c281b515986d94fa015b401c9699
Alexandro <38000075+Kalejo@users.noreply.github.com>
2026-09-14 22:21:56 +0200

    Ajout de la passerelle Internet

--- network.tf
+++ network.tf
@@ -7,3 +7,13 @@ resource "aws_vpc" "main" {
     Name = "${var.project_name}-vpc"
   }
 }
+
+
+# Passerelle reliant le VPC à Internet
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.project_name}-igw"
+  }
+}
